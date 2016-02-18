8170cd5515891fb78eb2f2cef10da8c3796fb515
unknown <falyanguzov@303-TCH.sch239.net>
2016-02-18 15:15:37 +0300

    Fixed transitions, gathered all data. Implemented transition to game view.

--- gamescene.py
+++ gamescene.py
@@ -4,6 +4,24 @@ Template for new module
 """
 
 from cocos.director import director
+from cocos import scene
+from cocos import tiles
+from cocos.layer import scrolling
+
+
+class Gamescene(scene.Scene):
+    
+    def __init__(self):
+        super().__init__()
+        level_map = tiles.load('test-field.tmx')
+        bg_layer = level_map['bg']
+        walls_layer = level_map['walls']
+        scroller = scrolling.ScrollingManager()
+        scroller.add(bg_layer)
+        scroller.add(walls_layer)
+        self.add(scroller)
+
+
 
 def main():
     """ Test scene """
--- menu.py
+++ menu.py
@@ -6,6 +6,10 @@ Menu scene
 from cocos.director import director
 from cocos import scene
 from cocos import menu
+from cocos.scenes import transitions
+
+
+import gamescene
 
 class Menu(scene.Scene):
     """ 
@@ -39,10 +43,12 @@ class MenuLayer(menu.Menu):
         items = []
         items.append(menu.MenuItem("New game", self.on_new_game))
         items.append(menu.MenuItem("Exit", self.on_exit))
-        self.create_menu(items, menu.zoom_in(), menu.zoom_out())
+        self.create_menu(items, menu.shake(), menu.shake_back())
 
     def on_new_game(self):
         """ Start new game """
+        director.push( \
+            transitions.MoveInRTransition(gamescene.Gamescene(), 2.0, director.scene))
     
     def on_exit(self):
         """ Exit game """
